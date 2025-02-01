667e2afb1eb4f437a839167661ee3a58c48df3f5
niyarin <inoanet@gmail.com>
2025-02-01 13:44:50 +0900

    Add additional test.

--- test/test.scm
+++ test/test.scm
@@ -8,5 +8,38 @@
 (check (rdr/read (open-input-string "some-symbol"))
        => 'some-symbol)
 
+(check (rdr/read (open-input-string "some-symbol?"))
+       => 'some-symbol?)
+
+(check (rdr/read (open-input-string "#\\a"))
+       => #\a)
+
+(check (rdr/read (open-input-string "#\\newline"))
+       => #\newline)
+
+;; list test
+
+(check (rdr/read (open-input-string "(foo bar baz)"))
+       => '(foo bar baz))
+
+
+(check (rdr/read (open-input-string "((foo bar) baz)"))
+       => '((foo bar) baz))
+
+;; delimiter test
+
+(check (rdr/read (open-input-string "#\\newline list"))
+       => #\newline)
+
+(check (rdr/read (open-input-string "#\\newline\nlist"))
+       => #\newline)
+
+
+;; double read test
+
+(let ((port (open-input-string "#\\newline\nlist")))
+  (check (rdr/read port) => #\newline)
+  (check (rdr/read port) => 'list))
+
 (check-report)
 
